a75b57104a27593c8b251863ac5e3a4268bdef02
crazedRomeo <crazedRomeo0305@gmail.com>
2021-12-22 21:17:40 +0700

    refactor code 1

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -3,14 +3,14 @@ import FooterBrand from "./footerBrand";
 export default function Footer() {
     return (
         <div>
-            <FooterBrand></FooterBrand>
+            <FooterBrand />
             <div id="section-footer">
                 <footer className="footer background-dark">
                     <div className="footer--content">
                         <div className="container footer--container media">
                             <div className="footer--block ">
                                 <a className="logo" href="">
-                                    <img className="logo--image" src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/3617585/settings-images/O14Sjd9QSvma4oiA4RfT-file.jpg" alt="Footer Logo"></img>
+                                    <img className="logo--image" src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/3617585/settings_images/O14Sjd9QSvma4oiA4RfT_file.jpg" alt="Footer Logo" />
                                 </a>
                             </div>
                             <div className="footer--block ">
@@ -20,21 +20,27 @@ export default function Footer() {
                             </div>
                             <div className="footer--block media--body">
                                 <div className="link-list justify-content-right">
-                                    <a className="link-list--link" href="https://www.cariber.co/privacy-policy" rel="noopener">นโยบายความเป็นส่วนตัว</a>
-                                    <a className="link-list--link" href="https://www.cariber.co/terms-conditions" rel="noopener">เงื่อนไขการใช้งาน</a>
+                                    <a className="link-list--link" href="https://www.cariber.co/privacy-policy" rel="noopener">
+                                        นโยบายความเป็นส่วนตัว
+                                    </a>
+                                    <a className="link-list--link" href="https://www.cariber.co/terms-conditions" rel="noopener">
+                                        เงื่อนไขการใช้งาน
+                                    </a>
                                 </div>
                             </div>
-                            <span style={{ color: '#fbf5e3' }}>contact@cariber.co</span>
-                            <div id="block-1612942017335" className="footer--block ">
+                            <span style={{ color: '#fbf5e3' }}>
+                                contact@cariber.co
+                                </span>
+                            <div className="footer--block ">
                                 <div className="social-icons social-icons--">
                                     <a className="social-icons--icon" href="https://www.facebook.com/cariberofficial">
-                                        <i className="fab fa-facebook-f"></i>
+                                        <i className="fab fa-facebook-f"/>
                                     </a>
                                     <a className="social-icons--icon" href="https://www.instagram.com/cariber.official/">
-                                        <i className="fab fa-instagram"></i>
+                                        <i className="fab fa-instagram"/>
                                     </a>
                                     <a className="social-icons--icon" href="https://www.linkedin.com/company/cariber/">
-                                        <i className="fab fa-linkedin-in"></i>
+                                        <i className="fab fa-linkedin-in"/>
                                     </a>
                                 </div>
                             </div>
--- frontend/pages/courses.tsx
+++ frontend/pages/courses.tsx
@@ -116,20 +116,20 @@ export default function Courses() {
 
     return (
         <div className="bg-img">
-            <Header></Header>
-            <div className="sizer ">
-                <div className="container ">
+            <Header/>
+            <div className="sizer">
+                <div className="container">
                     <div className="row align-items-center">
-                        <div kjb-settings-id="sections_1612934128394_blocks_1637758931963_settings_width" id="block-1637758931963" className="block-type--image text-col-12hidden--mobile" data-reveal-event="" data-reveal-offset="" data-reveal-units="seconds">
+                        <div className="block-type--image text-col-12hidden--mobile">
                             <div className="block box-shadow-none background-unrecognized aos-init aos-animate" data-aos="none" data-aos-delay="0" data-aos-duration="0">
                                 <div className="image">
-                                    <a className="image__link" href="https://checkout.cariber.co/?add-to-cart=685&amp;cfp=bGFyZ2ViYW5uZXJfY291cnNlcw==">
-                                        <img className="image__image" src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/3617585/settings_images/WwCSkYs4TKWw0zpTBGtQ_file.jpg" kjb-settings-id="sections_1612934128394_blocks_1637758931963_settings_image" alt=""></img>
+                                    <a className="image--link" href="https://checkout.cariber.co/?add-to-cart=685&amp;cfp=bGFyZ2ViYW5uZXJfY291cnNlcw==">
+                                        <img className="image--image" src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/3617585/settings_images/WwCSkYs4TKWw0zpTBGtQ_file.jpg" alt=""></img>
                                     </a>
                                 </div>
                             </div>
                         </div>
-                        <div kjb-settings-id="sections_1612934128394_blocks_1614761448848_settings_width" id="block-1614761448848" className="block-type--text text-left col-12" data-reveal-event="" data-reveal-offset="" data-reveal-units="seconds">
+                        <div className="block-type--text text-left col-12">
                             <div className="block box-shadow-none background-unrecognized aos-init aos-animate" data-aos="none" data-aos-delay="0" data-aos-duration="0">
                                 <h2 style={{ textAlign: 'center' }}><span style={{ color: '#e74e25' }}>คอร์สทั้งหมด</span></h2>
                             </div>
@@ -137,14 +137,14 @@ export default function Courses() {
                         <div className="block-break"></div>
                         {features.map((v, i) => {
                             return (
-                                <div key={i} kjb-settings-id="sections_1612934128394_blocks_1612934128393_1_settings_width" id="block-1612934128393_1" className="block-type--feature text-center col-3">
+                                <div key={i} className="block-type--feature text-center col-3">
                                     <div className="block box-shadow-large background-light aos-init aos-animate" data-aos="none" data-aos-delay="0" data-aos-duration="0" style={{backgroundColor: "#ffffff", borderRadius: "4px"}}>
-                                        <div kjb-settings-id="sections_1612934128394_blocks_1612934128393_1_settings_width" id="block-1612934128393_1" className="block-type--featuretext-centercol-3" data-reveal-event="" data-reveal-offset="" data-reveal-units="seconds">
+                                        <div className="block-type--featuretext-centercol-3">
                                             <div className="feature">
                                                 <a href={v.link}>
-                                                    <img className="feature__image" src={v.img} kjb-settings-id="sections_1612934128394_blocks_1612934128393_1_settings_image" alt=""></img>
+                                                    <img className="feature--image" src={v.img} alt=""></img>
                                                 </a>
-                                                <div className="feature__text" kjb-settings-id="sections_1612934128394_blocks_1612934128393_1_settings_text">
+                                                <div className="feature--text">
                                                     <h5 style={{ textAlign: 'center' }}><a href="/thakorn-piyapan"><span style={{ color: '#223f99' }}><strong>{v.name}</strong></span></a></h5>
                                                     <h6 style={{ textAlign: 'center' }}><a href="/thakorn-piyapan"><strong>{v.career}</strong></a></h6>
                                                 </div>
