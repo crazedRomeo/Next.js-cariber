43ff020f09e7a38dbfc8be33fc13cdb6e7be3eb8
crazedRomeo <crazedRomeo0305@gmail.com>
2021-12-24 12:05:38 +0700

    devloped page "review" first card

--- frontend/pages/review.tsx
+++ frontend/pages/review.tsx
@@ -39,6 +39,50 @@ export default function Review() {
                     </div>
                 </div>
             </div>
+            <div id="block-person-1">
+                <div className="container">
+                    <div className="frame align-items-center justify-content-center">
+                        <div className="block-type--image text-col-11">
+                            <div className="row block box-shadow-large background-light aos-init aos-animate" >
+                                <Image className="image-image"
+                                    src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/2148484577/settings_images/bPYsO3kwQHOr4QyXSv7K_123288476_3453648371357312_9054237649683120135_n.webp"
+                                    alt=""
+                                    width={200}
+                                    height={200} />
+                                <div className="block-text">
+                                    <p style={{ marginBottom: 14 }}>
+                                        <strong>
+                                            <span style={{ color: '#2c3e50' }}>
+                                                &quot;ถ้าข้าพเจ้ามีเวลา 6 ชั่วโมงในการตัดต้นไม้ ข้าพเจ้าจะใช้เวลา 4 ชั่วโมงแรกลับขวานให้คม&quot;
+                                                <br />
+                                                เป็นวาทะของ&quot;อับราฮัม ลินคอล์น&quot; อดีตผู้นำสหรัฐอเมริกา
+                                                <br />
+                                                <br />
+                                                วิกฤติโควิด-19 ครั้งนี้หนักหนาสาหัสมาก
+                                                <br />
+                                                แต่วันหนึ่งมันต้องผ่านไป
+                                                <br />
+                                                และเมื่อวันที่ฟ้าเปิด ท้องฟ้าสดใส
+                                                <br />
+                                                ใครที่ลับขวานได้คมกว่าก็จะได้เปรียบ
+                                            </span>
+                                        </strong>
+                                    </p>
+                                    <h5 style={{ marginBottom: 0 }}>
+                                        <strong className="mt-auto">
+                                            <span>
+                                                <a href="https://www.facebook.com/118209918234524/posts/4271535732901901/?d=n" style={{ color: "#1e4397", fontSize: 16 }}>
+                                                    Facebook page : หนุ่มเมืองจันท์
+                                                </a>
+                                            </span>
+                                        </strong>
+                                    </h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Footer />
         </div >
     )
