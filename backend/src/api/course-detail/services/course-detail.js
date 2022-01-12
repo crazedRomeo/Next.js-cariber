d4127c3ee906f68f9a9610bd9e1f116671f6f40f
crazedRomeo <crazedRomeo0305@gmail.com>
2022-01-12 17:38:22 +0700

    add course detail content type

---
+++ backend/src/api/course-detail/services/course-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * course-detail service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::course-detail.course-detail');
