08480b422c8dd44bd2131169651903213f2917b0
crazedRomeo <crazedRomeo0305@gmail.com>
2022-02-01 23:16:40 +0700

    [Improve] polling readytostream

--- backend/src/plugins/upload-cloudflare/admin/src/pages/HomePage/index.js
+++ backend/src/plugins/upload-cloudflare/admin/src/pages/HomePage/index.js
@@ -1,25 +1,42 @@
-/*
- *
- * HomePage
- *
- */
-
-import React, { useRef, useState } from 'react';
-// import PropTypes from 'prop-types';
-
-import pluginId from '../../pluginId';
+import React, { useRef, useState, useEffect } from 'react';
+import { Button } from '@strapi/design-system/Button';
 import axios from 'axios';
-import PlayerPage from "../../components/PlayerPage";
 import { Stream } from '@cloudflare/stream-react';
 const HomePage = () => {
-  const [videolink, setVideoLink] = useState("e8cc1cede48c8f076352121fd48f5e69")
+  const [videolink, setVideoLink] = useState()
   const [signedtoken, setSignedToken] = useState()
+  const [fileupload, setFileUpload] = useState()
+  const [readyToStream, setReadyToSteam] = useState(false)
+  const [intervalID,setIntervalID] = useState()
   const account = '05c1fb4f378588a2c03f1d5db10d3123';
   const token = '7UwW6lh1a95WzpIRXun1ZV-b_fRzi1CopJbQ4P_3'
-  const testFunc = ($event) => {
-    console.log("test test", $event);
+  const firstUpdate = useRef(true);
+  useEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+  });
+
+  useEffect(() => {
+    
+    if (!firstUpdate.current){
+      const interval = videolink ? setInterval(getVideoStatus, 3000): ""
+      setIntervalID(interval)
+      if (readyToStream && videolink) {
+        clearInterval(intervalID);
+        getSignedToken()
+      }
+    }
+  }, [videolink, readyToStream]); 
+  
+  const handleUpload = ($event) => {
+    setFileUpload($event.target.files[0])
+  }
+  const testFunc = () => {
+    setReadyToSteam(false)
     const fd = new FormData()
-    fd.append("file",$event.target.files[0])
+    fd.append("file",fileupload)
     fd.append('requireSignedURLs', true)
     axios({
       method: 'POST',
@@ -30,11 +47,23 @@ const HomePage = () => {
       }
     }).then(res=>{
       setVideoLink(res.data.result.uid)
-      getSignedToken()
     })
     // todo: upload with tus for > 200 mb
-    // Bug : after upload always get older (-1) videolink
-    // setVideoLink($event.target.value)
+  }
+  
+  const getVideoStatus = () => {
+    if ( !readyToStream ) {
+      axios({
+        method: 'GET',
+        url: `https://api.cloudflare.com/client/v4/accounts/${account}/stream/${videolink}`,
+        headers: {
+          "Authorization": `Bearer ${token}`
+        }
+      }).then(res=>{
+        setReadyToSteam(res.data.result.readyToStream)
+      }).catch(res => {
+      })
+    }
   }
   const getSignedToken = () => {
     axios({
@@ -50,10 +79,13 @@ const HomePage = () => {
   }
   return (
     <div>
-      <label for="myFile">{name}</label>
-      <input type="file" id="myFile" name="filename" onChange={testFunc}/>
-      {signedtoken}
-        <Stream key={signedtoken} controls src={signedtoken}/>
+      <label for="myFile">Upload Video</label>
+      <input type="file" id="myFile" name="filename" accept="video/*" onChange={handleUpload}/> <br></br>
+      <Button variant='default' onClick={testFunc} >Click to upload</Button><br/>
+      Link Video : {videolink}<br></br>
+      <div style={{width: '600px', height: '400px'}}>
+        { (readyToStream && videolink) ? <Stream key={signedtoken} controls src={signedtoken}/> : "Waiting video to ready for stream ..." }
+      </div>
     </div>
   );
 };
