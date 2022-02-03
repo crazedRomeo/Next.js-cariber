e5518e2ccc4d2a42065f9db807502103971f9549
crazedRomeo <crazedRomeo0305@gmail.com>
2022-02-03 14:56:36 +0700

    change api to strapi-dev

--- frontend/models/content.ts
+++ frontend/models/content.ts
@@ -1,4 +1,4 @@
-export const strapi = "http://localhost:1337";
+export const strapi = "https://strapi-dev.cariber.co";
 export const strapiApi = strapi + "/api";
 export const strapiAuthApi = strapiApi+"/auth/local";
 export const strapiRegisterApi = strapiApi+"/auth/local/register";
