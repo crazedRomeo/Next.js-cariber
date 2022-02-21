b09cce39a101899d4172b2a4d6e6fd4a8c53d5aa
crazedRomeo <crazedRomeo0305@gmail.com>
2022-02-21 12:58:46 +0700

    [Chaneg] ENV AUTH

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,10 @@ const path = require('path')
 require("dotenv").config();
 
 module.exports = {
+  env: {
+    NEXTAUTH_URL: process.env.NEXT_PLUBLIC_NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXT_PLUBLIC_NEXTAUTH_SECRET
+  },
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
