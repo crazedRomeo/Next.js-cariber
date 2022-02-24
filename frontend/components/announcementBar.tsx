d8269c7238973455a1ac91d286bf1018cec7c0ad
crazedRomeo <crazedRomeo0305@gmail.com>
2022-02-24 17:56:06 +0700

    this one

--- frontend/components/announcementBar.tsx
+++ frontend/components/announcementBar.tsx
@@ -3,14 +3,20 @@ import announcementBarApi from "../apiStrapi/announcementBarApi";
 
 export default function AnnouncementBar() {
   const [text, setText] = useState("");
-  announcementBarApi().then((value)=>{
+  announcementBarApi().then((value) => {
     setText(value.data?.attributes?.message)
   })
-  return (
-    <div className="announcement-bar text-center">
-      <p className="p-4 m-0">
-      {text}
-      </p>
-    </div>
+  if (text) {
+    return (
+      <div className="announcement-bar text-center">
+        <p className="p-4 m-0">
+          {text}
+        </p>
+      </div>
+    )
+  }
+  
+  return(
+    <div></div>
   )
 }
