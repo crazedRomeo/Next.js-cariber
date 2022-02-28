44d94ef3b9ce70bb19afafa2f78db167b8d232fc
crazedRomeo <crazedRomeo0305@gmail.com>
2022-02-28 23:09:19 +0700

    fixed cannot get message from BE

--- frontend/services/WoocommerceService.ts
+++ frontend/services/WoocommerceService.ts
@@ -1,6 +1,6 @@
 import { WoocommerceCredentials } from "../apiNest/models/content/woocommerce";
 import { NEST_API_URLS, nestHeaderAuth } from "../apiNest/models/contact";
-import {notification} from "antd";
+import { notification } from "antd";
 import router from "next/router";
 import { validateTokenApi } from "../apiNest/authApi";
 
@@ -26,32 +26,29 @@ export class WoocommerceService {
 
   private static async checkClaimedOrNot(credentials: WoocommerceCredentials) {
     try {
-      const response = await fetch(NEST_API_URLS.users + `/claim-shopee-id`, {
+      const response: { message: string } = await fetch(NEST_API_URLS.users + `/claim-shopee-id`, {
         method: "POST",
         headers: nestHeaderAuth(),
         body: JSON.stringify(credentials),
-      })
-      this.redirect('success');
-      return await response.json();
+      }).then(function(response) {
+        return response.json();
+      }).then(function(data) {
+        return data;
+      });
+      this.redirect(response.message);
+      return;
     } catch (error) {
       console.log(error);
     }
   }
 
-  private static redirect(type:string) {
-    if (type === 'success') { //response === 'Shopee ID has claimed'
-      notification['success']({message: 'Successfully Claimed'});
-      setTimeout( () => {
-        // show notification for 3 s
-        router.replace("/library").then(() => {});
-      }, 3000);
-      return;
-    }
-    notification['error']({message: 'This Course has been Claimed'});
+  private static redirect(type: string) {
+    type === 'Shopee ID has claimed'
+      ? notification['success']({message: 'Successfully Claimed'})
+      : notification['error']({message: 'This Course has been Claimed'});
     setTimeout( () => {
-      // show notification for 3 s
       router.replace("/library").then(() => {});
-    }, 3000);
+    }, 1000);
   }
 
 }
