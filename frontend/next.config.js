18a67f5a7278b235dab50ca25911ea8f39bf5ba2
crazedRomeo <crazedRomeo0305@gmail.com>
2022-03-11 13:59:01 +0700

    set sentry auth token

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -17,7 +17,10 @@ const moduleExports = {
 };
 
 const sentryWebpackPluginOptions = {
+  authToken: process.env.NEXT_PUBLIC_SENTRY_AUTH_TOKEN || "7c1da6903aea47e893dd30962e9b717c8fa246449e804163bdc9805a212ebb72",
   silent: true,
+  include: ".",
+  ignore: ["node_modules", "webpack.config.js"],
 };
 
 module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
