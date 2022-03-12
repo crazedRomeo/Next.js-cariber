3332d07f716b1173468b4dfa68d0f75cf0db4986
crazedRomeo <crazedRomeo0305@gmail.com>
2022-03-13 00:00:08 +0700

    thumbnail image is not declared fixed

--- frontend/apiNest/models/content/courseLms.ts
+++ frontend/apiNest/models/content/courseLms.ts
@@ -95,7 +95,7 @@ export class Evaluation {
 export class Certificate {
 	id: number;
 	name: string;
-	thumbnails?: string;
+	thumbnail_image: string;
 	type: ShowingType;
 	episode_number: number;
 	duration?: number;
@@ -103,7 +103,7 @@ export class Certificate {
 	constructor() {
 		this.id = 0;
 		this.name = 'Download Certificate';
-		this.thumbnails = '';
+		this.thumbnail_image = '';
 		this.type = ShowingType.certificate;
 		this.episode_number = 0;
 	}
