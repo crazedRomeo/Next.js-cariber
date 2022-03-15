b2bbacc188690d768e35e72bcaf1977d990279bf
crazedRomeo <crazedRomeo0305@gmail.com>
2022-03-15 13:34:21 +0700

    fix video continue countdown

---
+++ frontend/components/countdown.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import useCountDown from "react-countdown-hook";
+
+export default function Countdown({ second, callback }: { second: number, callback: () => void }) {
+  const initialTime = second * 1000;
+  const interval = 1000;
+  const [timeLeft, { start, pause, resume, reset }] = useCountDown(initialTime, interval);
+  const [isStart, setIsStart] = useState(false);
+
+  useEffect(() => {
+    start();
+    setIsStart(true);
+  }, []);
+
+  if (timeLeft === 0 && isStart) {
+    callback();
+    return(<></>)
+  }
+
+  return (
+    <>
+      {timeLeft ? timeLeft / 1000 : timeLeft}
+    </>
+  )
+}
